348f86ba4b96dfc5e56ab93a772a20d1268c9aba
kaigth <kaigth@hotmail.com>
2012-03-16 10:16:52 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teamber::Application.config.secret_token = '2b419a785203766bfc4fb53c6da60503a53e39d3f5ca2a9a2b86c1d2d14fac8e77109ce58815a129b27c77468877eb801373916233089a237e0ae38bfb4057e2'
